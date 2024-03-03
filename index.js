3190cbe75506b0f2000b2d6eb93554610ef8dbcd
panmelakaren1 <32483061-panmelakaren1@users.noreply.replit.com>
2024-03-03 01:24:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+
+var v1 = prompt("Digite o primeiro número:");
+var v2 = prompt("Digite o segundo número:");
+var v3 = prompt("Digite o terceiro número:");
+
+
+console.log("Os números digitados são: " + v1 + ", " + v2 + " e " + v3);
